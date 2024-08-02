81201f3fcfa6f43ffe0ee51ed80ac06ab2ec9f88
leehee16 <hojlgg4@gmail.com>
2024-08-02 17:35:43 +0900

    하나는 완성본, 하나는 가상환경을 새로 만듬

--- voicebot_stt_tts.py
+++ voicebot_stt_tts.py
@@ -1,10 +1,16 @@
 #스트림릿 임포트
 import streamlit as st
+from audiorecorder import audiorecorder
 
 # OpenAI 패키지 추가
 import openai
 import os
+
 from dotenv import load_dotenv
+
+# 시간 정보를 위한 패키지 추가
+from datetime import datetime
+
 
 # env 경로 지정
 try:
@@ -28,6 +34,24 @@ try:
     print("OpenAI 클라이언트를 성공적으로 설정했습니다.")
 except Exception as e:
     print(f"OpenAI 클라이언트를 설정하는 중 오류가 발생했습니다: {e}")
+
+def STT(speech):
+    # 파일 저장
+    filename='input.mp3'
+    speech.export(filename, format="mp3")
+
+    # 음원 파일 열기
+    with open(filename, "rb") as audio_file:
+        # Whisper 모델을 활용해 텍스트 얻기
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1", 
+            file=audio_file
+        )
+
+    # 파일 삭제
+    os.remove(filename)
+    
+    return transcription.text
 
 ##### 메인 함수 #####
 def main():
@@ -82,11 +106,36 @@ def main():
         # 왼쪽 영역 작성
         st.subheader("질문하기")
 
+
+        # 음성 녹음 아이콘 추가
+        audio = audiorecorder()
+        if (audio.duration_seconds > 0) and (st.session_state["check_reset"]==False):
+            # 음성 재생 
+            st.audio(audio.export().read())
+
+            # 음원 파일에서 텍스트 추출
+            question = STT(audio)
+
+            # 채팅을 시각화하기 위해 질문 내용 저장
+            now = datetime.now().strftime("%H:%M")
+            st.session_state["chat"] = st.session_state["chat"] + [("user", now, question)]
+
+            # GPT 모델에 넣을 프롬프트를 위해 질문 내용 저장
+            st.session_state["messages"] = st.session_state["messages"] + [{"role": "user", "content": question}]
     with col2:
         # 오른쪽 영역 작성
         st.subheader("질문/답변")
+
+        
 
 #실행
 if __name__ == "__main__" :
     print(__name__)
     main()
+
+import os
+print(os.listdir('/opt/anaconda3/lib/python3.12/site-packages/audiorecorder'))
+
+
+import audiorecorder
+print(audiorecorder.__file__)
